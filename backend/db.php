f856ea3809329859ecf31c0925d9d773fb154e90
Julio_DMG <juliodmg0812@gmail.com>
2026-01-25 18:49:10 -0500

    fixed some bugs in the db and login files

--- backend/db.php
+++ backend/db.php
@@ -1,7 +1,7 @@
 <?php
 //conect to local database
-    $conn = mysqli_connect("localhost","theBeast","WeloveCOP4331","COP4331"); //variable
-    if (mysqli_connect_errno()) //check if connection was succesful
+    $conn = mysqli_connect("localhost","TheBeast","WeLoveCOP4331","COP4331"); //variable
+    if ($conn->connect_error) //check if connection was succesful
     {
         die(json_encode(["error"=> $conn->connect_error])); //stops execution and returns an error message if no conection was stablished (json file)
     }
