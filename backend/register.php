5c2011a85021ab2efc9540a4b2d64fe56c8ca6f2
Julio_DMG <juliodmg0812@gmail.com>
2026-01-29 21:16:11 -0500

    fixed some bugs and added a safe case

--- backend/register.php
+++ backend/register.php
@@ -1,16 +1,22 @@
 <?php
-require_once("db");
+require_once("db.php");
 
-$_indata= json_decode(file_get_contents("php://input"),true);
+$indata = json_decode(file_get_contents("php://input"),true);
+//check for data
+if (!$indata)
+{
+    die(json_encode(["error" => "Invalid JSON input"]));
+}
+
 $stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES (?,?,?,?)");
-$stmt->bind_param("ssss", $_indata["firstname"],$_indata["lastname"],$_indata["Login"],$_indata["Password"]);
-if( $stmt->execute() )
-    {
-        echo json_encode(["error"=>""]);
-    } else 
-    {
-        echo json_encode(["error"=> $stmt->error]);
-    }
-    $stmt->close();
-    $conn->close();
-    ?>
+$stmt->bind_param("ssss", $indata["firstName"],$indata["lastName"], $indata["login"],$indata["password"]);
+if($stmt->execute())
+{
+    echo json_encode(["error"=>""]);
+}else 
+{
+    echo json_encode(["error"=> $stmt->error]);
+}
+$stmt->close();
+$conn->close();
+?>
